986f3b25d70f2f3b3212e84dd170313c665a4056
dominiczhu <dominiczhu@tencent.com>
2022-03-29 16:24:23 +0800

    sleuth初体验并且修改了loadbalancer的warn

---
+++ spring/spring-cloud/spring-cloud-sleuth/hello-spring-cloud-sleuth/hello-spring-cloud-sleuth-get-started/src/test/java/org/talk/is/cheap/spring/cloud/sleuth/test/tracer/TestTracer.java
@@ -0,0 +1,11 @@
+package org.talk.is.cheap.spring.cloud.sleuth.test.tracer;
+
+/**
+ * @author dominiczhu
+ * @version 1.0
+ * @title TestTracer
+ * @date 2022/3/29 1:27 下午
+ */
+
+public class TestTracer {
+}
