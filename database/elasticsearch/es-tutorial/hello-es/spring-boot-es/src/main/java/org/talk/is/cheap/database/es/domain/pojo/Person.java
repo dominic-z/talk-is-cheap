54fac8f997442de6242958904f94612093031396
dominiczhu <dominiczhu@tencent.com>
2022-03-30 17:37:24 +0800

    es doc

---
+++ database/elasticsearch/es-tutorial/hello-es/spring-boot-es/src/main/java/org/talk/is/cheap/database/es/domain/pojo/Person.java
@@ -0,0 +1,16 @@
+package org.talk.is.cheap.database.es.domain.pojo;
+
+import lombok.Data;
+
+/**
+ * @author dominiczhu
+ * @version 1.0
+ * @title Person
+ * @date 2022/3/30 4:42 下午
+ */
+@Data
+public class Person {
+
+    private String name;
+    private Integer age;
+}
