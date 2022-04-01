9d969cf424e7dd2ca217719b6392458c910cda11
dominiczhu <dominiczhu@tencent.com>
2022-04-01 10:22:04 +0800

    learn/20220331-spring-cloud-ribbon

---
+++ spring/spring-cloud/spring-cloud-ribbon/hello-spring-cloud-ribbon/hello-spring-cloud-ribbon-server-1001/src/main/java/org/talk/is/cheap/spring/cloud/ribbon/eureka/controller/BackendController.java
@@ -0,0 +1,28 @@
+package org.talk.is.cheap.spring.cloud.ribbon.eureka.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author dominiczhu
+ * @version 1.0
+ * @title FrontendController
+ * @date 2022/3/31 1:13 下午
+ */
+@RestController
+@RequestMapping("/backend")
+@Slf4j
+public class BackendController {
+
+    private static final String LOG_MARKER = "[BackendController]";
+
+    @GetMapping("/echo")
+    public String echo(@RequestParam("value") String value) {
+        log.info("{} echo value: {}", LOG_MARKER, value);
+        return LOG_MARKER + value+"server1001";
+    }
+
+}
