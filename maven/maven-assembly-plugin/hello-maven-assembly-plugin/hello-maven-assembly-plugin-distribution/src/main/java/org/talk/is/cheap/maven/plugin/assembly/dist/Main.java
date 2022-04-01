1a8bdf6dd6bef77471aff6579f8b32ec486ac822
dominiczhu <dominiczhu@tencent.com>
2022-04-01 17:45:50 +0800

    maven-assembly-plugin

---
+++ maven/maven-assembly-plugin/hello-maven-assembly-plugin/hello-maven-assembly-plugin-distribution/src/main/java/org/talk/is/cheap/maven/plugin/assembly/dist/Main.java
@@ -0,0 +1,16 @@
+package org.talk.is.cheap.maven.plugin.assembly.dist;
+
+import com.google.common.collect.Lists;
+
+/**
+ * @author dominiczhu
+ * @version 1.0
+ * @title Main
+ * @date 2022/4/1 3:47 下午
+ */
+public class Main {
+
+    public void show(){
+        Lists.newArrayList("123");
+    }
+}
---
+++ maven/maven-assembly-plugin/hello-maven-assembly-plugin/readme.md
@@ -0,0 +1,5 @@
+# doc
+文档写的挺全的，有问题去查就好
+[assembly配置文件](https://maven.apache.org/plugins/maven-assembly-plugin/assembly)
+
+[Maven 教程（20）— 使用maven-assembly-plugin插件来定制化打包](https://blog.csdn.net/liupeifeng3514/article/details/79777976)
