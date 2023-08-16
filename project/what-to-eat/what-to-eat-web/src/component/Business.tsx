55f19803ab1391d52608bc9bbebe181471510939
dominic_starry <dominic_starry@outlook.com>
2023-08-16 21:12:35 +0800

    前段雏形

---
+++ project/what-to-eat/what-to-eat-web/src/component/Business.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../css/Business.css"
+
+export default function Business(){
+    const defaultImgUrl="https://ts1.cn.mm.bing.net/th/id/R-C.0bab160015bf9e56450d5fded33bd448?rik=VFMstq3mAZG1pg&riu=http%3a%2f%2fwww.qzqn8.com%2fwp-content%2fuploads%2f2020%2f02%2f3-9.jpg&ehk=pnkhUlGCDYmxisEuCwYL4zXW92froAxt%2f0B5i9AYAkY%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1";
+    const defaultName = "这是小猫店";
+    const defaultDescription = "小猫店天天做活动，然后到处拉大便，拉得满屏幕都是";
+    return (
+        <div className='business'>
+            <div className='avatar'>
+                <img src={defaultImgUrl}></img>
+            </div>
+
+            <div className='businessInfo'>
+                <div className='name'>
+                    {defaultName}
+                </div>
+                <div className='description'>
+                    {defaultDescription}
+                    {defaultDescription}
+                    {defaultDescription}
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ project/what-to-eat/what-to-eat-web/src/component/BusinessList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/BusinessList.css'
+import Business from './Business';
+export default function BusinessList() {
+
+    return (
+        <div className='businessList'>
+            <Business></Business>
+            <Business></Business>
+            <Business></Business>
+            <Business></Business>
+            <Business></Business>
+        </div>
+
+    )
+
+}
