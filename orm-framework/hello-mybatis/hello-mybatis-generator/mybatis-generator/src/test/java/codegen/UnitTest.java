550e663778d7949480acbba9101e2af680709f2d
dominic_starry <dominic_starry@outlook.com>
2024-01-18 23:24:37 +0800

    修改了mbg，配置文件改为yaml

---
+++ orm-framework/hello-mybatis/hello-mybatis-generator/mybatis-generator/src/test/java/codegen/UnitTest.java
@@ -0,0 +1,56 @@
+package codegen;
+
+import codegen.config.CodeGeneratorConfig;
+import lombok.Data;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+@Slf4j
+public class UnitTest {
+    @Getter
+    public static class TestConfig {
+        private int v1;
+
+        private int v2;
+
+        @Data
+        public static class P {
+            private String v1;
+            private int v2;
+        }
+
+        private P p1;
+    }
+
+    @Test
+    public void readYamlTest() throws IOException {
+
+        InputStream inputStream = UnitTest.class.getClassLoader().getResourceAsStream("test-config.yml");
+        inputStream.mark(Integer.MAX_VALUE);
+        Object rawYaml = new Yaml().load(inputStream);
+        log.info("raw yaml, {}", rawYaml);
+        inputStream.reset();
+        TestConfig config = new Yaml().loadAs(inputStream, TestConfig.class);
+        log.info("class yaml, {}", config);
+
+
+    }
+
+
+    @Test
+    public void testReadConfig() {
+//        CodeGeneratorConfig.setConfigPath("src/main/resources/config.yml");
+        log.info("{}", CodeGeneratorConfig.getInstance());
+    }
+
+}
