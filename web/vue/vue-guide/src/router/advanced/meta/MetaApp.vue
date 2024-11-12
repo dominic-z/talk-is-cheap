12125fd1ec9d9c2b94131c4e26f82f16842a59d7
dominic_starry <dominic_starry@outlook.com>
2024-11-12 22:18:54 +0800

    进度更新

---
+++ web/vue/vue-guide/src/router/advanced/meta/MetaApp.vue
@@ -0,0 +1,7 @@
+<template>
+
+    <RouterLink to="/posts">/posts</RouterLink>
+    <RouterLink to="/posts/new">/posts/new</RouterLink>
+    <RouterLink to="/posts/12">/posts/12</RouterLink>
+    <RouterView></RouterView>
+</template>
---
+++ web/vue/vue-guide/src/router/advanced/navigation-guards/Avatar.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onBeforeRouteLeave, onBeforeRouteUpdate } from 'vue-router'
+import { useRoute } from 'vue-router';
+import { watch } from 'vue';
+
+
+</script>
+
+<template>
+    <div>
+        Avatar: {{ $route.fullPath }}
+    </div>
+</template>
