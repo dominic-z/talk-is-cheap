c2ec36a74d1a8e284e63673265cab5ef0f9db4c9
dominic_starry <dominic_starry@outlook.com>
2025-03-22 16:43:45 +0800

    maven-resources-plugin

--- spring/spring-boot/hello-spring-boot/hello-spring-boot-application/spring-boot-composition/src/main/java/com/example/springboot/hellospringboot/HelloSpringBootApplication.java
+++ spring/spring-boot/hello-spring-boot/hello-spring-boot-application/spring-boot-composition/src/main/java/com/example/springboot/hellospringboot/HelloSpringBootApplication.java
@@ -4,18 +4,28 @@ import com.example.springboot.hellospringboot.dao.jdbc.template.ItemJdbcDao;
 import lombok.extern.slf4j.Slf4j;
 import org.example.spring.starter.log.Loggers;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+// 在run config里配置--spring.profiles.active=dev，就会使用application-dev.yml
 @SpringBootApplication
 // @SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) 如果没有手动配置datasource，那么就需要关闭数据源自动配置
 @Slf4j(topic = "app_root")
 @EnableFeignClients
 @EnableDiscoveryClient
 public class HelloSpringBootApplication implements CommandLineRunner {
+
+    @Value("${hello-world}")
+    private String helloWordText;
+
+
+    @Value("${env.prop}")
+    private String envProp;
 
     public static void main(String[] args) {
         SpringApplication.run(HelloSpringBootApplication.class, args);
@@ -32,5 +42,7 @@ public class HelloSpringBootApplication implements CommandLineRunner {
 //		log.error("test e",new RuntimeException("error test"));
         Loggers.ERROR_LOG.error("error_log test log back error", new RuntimeException());
         log.error("test log back error", new RuntimeException());
+        log.info("helloWordText: {}", this.helloWordText);
+        log.info("envProp: {}", this.envProp);
     }
 }
