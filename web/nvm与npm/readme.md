2ddf1d47ccc8810556635364b681449ef1ab2d5c
dominic_starry <dominic_starry@outlook.com>
2025-03-30 17:50:59 +0800

    一些js小知识

---
+++ web/nvm与npm/readme.md
@@ -0,0 +1,14 @@
+# npm与nvm
+npm是个前端服务的包管理器，npm有一堆版本，就像java有各种版本一样。nvm就是允许你电脑上安装多个npm版本，并且支持自由切换。
+
+# 使用nvm安装某个版本
+```shell
+
+nvm install v22.14.0
+nvm use v22.14.0
+
+# 装个cnpm 默认使用阿里镜像，很快
+npm install -g cnpm --registry=https://registry.npmmirror.com
+
+```
+
---
+++ web/vue/vue3-playground/src/components/LocalStorageDemo.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+let testValue=null;
+if (typeof(Storage) !== "undefined") {
+    // 支持 localStorage
+    localStorage.setItem('test', 'testValue');
+    testValue = localStorage.getItem('test');
+    
+    console.log(testValue);
+} else {
+    // 不支持 localStorage
+    console.log('Sorry, your browser does not support Web Storage...');
+}
+</script>
+
+<template>
+    <p>
+        {{ testValue }}
+    </p>
+
+</template>
