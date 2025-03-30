e08ee4d006fd61f2642a06074296a32e563c8cc3
dominic_starry <dominic_starry@outlook.com>
2025-03-30 22:25:46 +0800

    mediasource

---
+++ spring/spring-boot/hello-spring-boot/hello-spring-boot-starter-web/static-resources/src/main/java/resources/controller/FileDownloadController.java
@@ -0,0 +1,46 @@
+package resources.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+@RestController
+@Slf4j
+public class FileDownloadController {
+
+    private static final String DOWNLOAD_DIR = "git_ignore";
+
+    @GetMapping("/download")
+    public ResponseEntity<Resource> downloadFile() {
+        File file = Paths.get(DOWNLOAD_DIR+"/SampleVideo_720x480_1mb.mp4").toFile();
+        Resource resource = new FileSystemResource(file);
+
+        if (!resource.exists()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+}
---
+++ web/html-js-css/MediaSource/readme.md
@@ -0,0 +1,9 @@
+# 说明
+
+mediasource的案例需要一个后端服务作为文件下载服务提供方，所以示例都放在了spring-boot/hello-spring-boot-starter-web之中的media-source.html之中。
+
+借助MSE的能力，将接收到的实时流通过 blob url 往video标签中灌入二进制数据（如fmp4格式流），或者使用 canvas 来实现直播。
+
+参考资料:
+1. [mdn mediasource](https://developer.mozilla.org/zh-CN/docs/Web/API/MediaSource#%E7%A4%BA%E4%BE%8B)
+2. [video 为什么有的视频地址是blob开头？](https://juejin.cn/post/7221865079168253989) ---好文，百度存一份
