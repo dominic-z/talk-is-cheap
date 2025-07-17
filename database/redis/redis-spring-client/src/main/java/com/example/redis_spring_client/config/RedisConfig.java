62637a565359006798bd7a94e49f25ead3d091e2
dominic_starry <dominic_starry@outlook.com>
2025-07-17 22:05:50 +0800

    redis+springboot

--- database/redis/redis-spring-client/src/main/java/com/example/redis_spring_client/config/RedisConfig.java
+++ database/redis/redis-spring-client/src/main/java/com/example/redis_spring_client/config/RedisConfig.java
@@ -1,16 +1,17 @@
 package com.example.redis_spring_client.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -34,6 +35,20 @@ public class RedisConfig {
                 .cacheDefaults(cacheConfig)
                 .build();
     }
+//
+//    @Bean
+//    public LettuceConnectionFactory redisConnectionFactory() {
+//        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration()
+//                .clusterNode("127.0.0.1", 6379);
+//
+//        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+//                .clientOptions(ClientOptions.builder()
+//                        .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+//                        .build())
+//                .build();
+//
+//        return new LettuceConnectionFactory(clusterConfig, clientConfig);
+//    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
@@ -42,6 +57,39 @@ public class RedisConfig {
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+//        等同于使用new GenericJackson2JsonRedisSerializer()，会将类型信息作为一个json的一个字段记录进redis
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        // 方法过期，改为下面代码
+////        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance ,
+//                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+//        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(objectMapper,Object.class);
+//        template.setValueSerializer(jackson2JsonRedisSerializer);
+
+//        对string类型的value使用字符串序列化器
+//        template.setValueSerializer(new StringRedisSerializer());
+
+//        对hash的key的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return template;
+    }
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+    @Value("${spring.data.redis.port}")
+    private String redisPort;
+    @Value("${spring.data.redis.password}")
+    private String redisPwd;
+
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress(String.format("redis://%s:%s",redisHost,redisPort)).setPassword(redisPwd);
+        config.setCodec(new JsonJacksonCodec());
+        return Redisson.create(config);
     }
 }
