408caa24cef1492d359f8480fce4610aa128c140
dominiczhu <dominic_starry@outlook.com>
2025-07-22 17:22:08 +0800

    nacos3的配置中心和注册中心测试

--- spring/spring-cloud/hello-spring-cloud-nacos/hello-spring-cloud-nacos-backend/src/main/java/org/example/backend/App.java
+++ spring/spring-cloud/hello-spring-cloud-nacos/hello-spring-cloud-nacos-backend/src/main/java/org/example/backend/App.java
@@ -1,6 +1,10 @@
 package org.example.backend;
 
 
+import lombok.extern.slf4j.Slf4j;
+import org.example.backend.config.ConfigFromNacos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,9 +13,33 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients //开启Feign远程调用功能
 @EnableDiscoveryClient // 开启服务发现功能
-public class App {
+@Slf4j
+public class App implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class,args);
+    }
+    @Autowired
+    ConfigFromNacos configFromNacos;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    log.info("config1: {}",configFromNacos.getInfo1());
+                    log.info("config2: {}",configFromNacos.getInfo2());
+                }
+            }
+        }).start();
     }
 }
