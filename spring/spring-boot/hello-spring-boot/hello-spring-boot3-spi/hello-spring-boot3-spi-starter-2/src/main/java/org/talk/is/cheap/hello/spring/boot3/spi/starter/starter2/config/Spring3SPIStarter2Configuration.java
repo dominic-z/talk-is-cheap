2d61ea3de7fc5f847779d3132fdd1a147f190691
dominiczhu <dominic_starry@outlook.com>
2025-08-04 10:05:15 +0800

    starter之间的先后顺序与condition关系

---
+++ spring/spring-boot/hello-spring-boot/hello-spring-boot3-spi/hello-spring-boot3-spi-starter-2/src/main/java/org/talk/is/cheap/hello/spring/boot3/spi/starter/starter2/config/Spring3SPIStarter2Configuration.java
@@ -0,0 +1,21 @@
+package org.talk.is.cheap.hello.spring.boot3.spi.starter.starter2.config;
+
+
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.talk.is.cheap.hello.spring.boot3.spi.common.pojo.Product;
+
+@Configuration
+@AutoConfigureAfter(name = "Spring3SPIStarterConfiguration")
+public class Spring3SPIStarter2Configuration {
+
+
+    @Bean(name = "another-product")
+    public Product product(){
+        System.out.println("starter2中的product创建");
+        return new Product();
+    }
+
+}
