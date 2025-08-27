8efb7b2e51fcc76537af577c5baf9c0e3799463d
dominic_starry <dominic_starry@outlook.com>
2025-08-27 23:15:53 +0800

    stagedetail视图。todo: 选中展示的card会触发mouseleave事件

---
+++ project/free-flow/free-flow-web/src/components/task-startup/StageStartupDetail.vue
@@ -0,0 +1,186 @@
+<script setup>
+import { mdiWindowClose } from '@mdi/js'
+import { ref } from 'vue'
+const emitter = defineEmits(['update:draw'])
+
+const props = defineProps(['draw'])
+const navWidth = ref(600)
+
+const items = ref([
+    {
+        name: 'aa',
+        calories: 'aa'
+    },
+    {
+        name: 'aa',
+        calories: 'aa'
+    },
+    {
+        name: 'aa',
+        calories: 'aa'
+    },
+    {
+        name: 'aa',
+        calories: 'aa'
+    },
+    {
+        name: 'aa',
+        calories: 'aa'
+    },
+    {
+        name: 'aa',
+        calories: 'aa'
+    },
+    {
+        name: 'aa',
+        calories: 'aa'
+    }, {
+        name: 'aa',
+        calories: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    },
+])
+
+
+const executions = ref([
+    {
+        'stageExecutionId': 1,
+        'launchTime': '2020-08-01 12:20:10',
+    },
+    {
+        'stageExecutionId': 2,
+        'launchTime': '2020-08-02 12:20:10',
+    },
+    {
+        'stageExecutionId': 3,
+        'launchTime': '2020-08-03 12:20:10',
+    },
+])
+
+
+const logs = ref([
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    '11'
+])
+</script>
+
+<template>
+
+    <v-navigation-drawer v-model="props.draw" :location="'right'" :width="navWidth" floating="">
+        <v-toolbar density="compact">
+
+            <v-btn :icon="mdiWindowClose" @click="$emit('update:draw')"></v-btn>
+        </v-toolbar>
+
+        <v-card class="border-thin">
+            <v-card-title>基本信息</v-card-title>
+            <v-card-text>
+                <v-table density="compact" height="100px" striped="even" fixed-header class="border-thin text-break">
+                    <!-- 列宽定义区域 -->
+                    <colgroup>
+                        <col style="width: 25%;"> <!-- 第 1 列宽度 -->
+                        <col style="width: 75%;"> <!-- 第 2 列宽度 -->
+                    </colgroup>
+                    <tbody>
+                        <tr>
+                            <td>version</td>
+                            <td>1.1</td>
+                        </tr>
+                        <tr>
+                            <td>Startup Time</td>
+                            <td>2021-07-01</td>
+                        </tr>
+                        <tr>
+                            <td>Retry Times</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </v-table>
+
+            </v-card-text>
+
+        </v-card>
+
+        <v-card class=" border-thin">
+            <v-card-title>启动参数</v-card-title>
+            <v-card-text>
+                <v-table density="compact" height="200px" striped="even" fixed-header class="border-thin text-break">
+                    <!-- 列宽定义区域 -->
+                    <colgroup>
+                        <col style="width: 25%;"> <!-- 第 1 列宽度 -->
+                        <col style="width: 75%;"> <!-- 第 2 列宽度 -->
+                    </colgroup>
+                    <thead>
+
+                        <tr class="text-h6">
+                            <th class="text-left">
+                                Parameter
+                            </th>
+                            <th class="text-left">
+                                Value
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="item in items" :key="item.name">
+                            <td>{{ item.name }}</td>
+                            <td>{{ item.calories }}</td>
+                        </tr>
+                    </tbody>
+                </v-table>
+            </v-card-text>
+        </v-card>
+
+        <v-divider></v-divider>
+
+        <v-card class="border-thin" :style="{ 'height': '100%' }">
+
+
+            <v-card-title>
+                执行信息
+            </v-card-title>
+
+            <!-- hide-details会隐藏掉一个小尾巴 -->
+            <v-select :items="executions" item-title="stageExecutionId" label="执行尝试" :hide-details="true">
+                <template v-slot:item="{ props, item }">
+                    <v-list-item v-bind="props" :subtitle="item.raw.launchTime"
+                        @click="() => console.log(props)"></v-list-item>
+                </template>
+            </v-select>
+
+
+            <v-card class=" border-thin">
+                <v-card-title>日志</v-card-title>
+                <v-card-text>
+                    <v-table density="compact" height="200px" striped="even" fixed-header
+                        class="border-thin text-break">
+                        <!-- 列宽定义区域 -->
+                        <colgroup>
+                            <col style="width: 25%;"> <!-- 第 1 列宽度 -->
+                            <col style="width: 75%;"> <!-- 第 2 列宽度 -->
+                        </colgroup>
+                        <thead>
+                            <tr >
+                                <th class="font-weight-bold">
+                                    时间
+                                </th>
+                                <th class="font-weight-bold">
+                                    内容
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in items" :key="item.name">
+                                <td>{{ item.name }}</td>
+                                <td>{{ item.calories }}</td>
+                            </tr>
+                        </tbody>
+                    </v-table>
+                </v-card-text>
+            </v-card>
+        </v-card>
+
+    </v-navigation-drawer>
+
+</template>
--- project/free-flow/free-flow-web/src/views/TaskStarupDetailView.vue
+++ project/free-flow/free-flow-web/src/views/TaskStarupDetailView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import TaskStartupDetailGraph from '@/components/task-startup/TaskStartupDetailGraph.vue';
+import { mdiArrowLeft } from '@mdi/js';
 import { mdiFolder } from '@mdi/js';
 import { mdiDotsVertical } from '@mdi/js';
 import { ref } from 'vue';
@@ -10,6 +11,22 @@ const retryCount = ref([3])
 const showTaskStartupDetail = ref(false)
 const rail = ref(true)
 
+function updateRail(e) {
+    if (e) {
+        // 收起nav
+        showTaskStartupDetail.value = false
+    }
+}
+
+function updateShowTaskStartupDetail(e) {
+    console.log(e)
+    if (showTaskStartupDetail.value) {
+        showTaskStartupDetail.value = false;
+    } else {
+        setTimeout(() => showTaskStartupDetail.value = true, 100)
+    }
+}
+
 </script>
 
 <template>
@@ -17,7 +34,7 @@ const rail = ref(true)
     <v-app>
         <v-app-bar color="primary" density="compact">
             <template v-slot:prepend>
-                <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-btn :icon="mdiArrowLeft"></v-btn>
             </template>
 
             <v-app-bar-title>Photos</v-app-bar-title>
@@ -28,7 +45,8 @@ const rail = ref(true)
         </v-app-bar>
 
 
-        <v-navigation-drawer expand-on-hover permanent rail @update:rail="(e) => rail = e">
+        <v-navigation-drawer permanent @update:rail="updateRail">
+        <!-- <v-navigation-drawer expand-on-hover permanent rail @update:rail="updateRail"> -->
             <v-divider></v-divider>
 
 
@@ -48,26 +66,8 @@ const rail = ref(true)
 
 
 
-                    <!-- 这是个啥玩意？解释一下，我的诉求是希望能够 -->
-                    <!-- 1. 在nav的hover之前展示icon，之后展开nav，展开后额外展示title -->
-                    <!-- 2. hover某一个item的时候，在这个item中展开一个card，显示这个item的详细信息 -->
-                    <!-- 参考文档中的原始代码，https://vuetifyjs.com/zh-Hans/components/navigation-drawers/#section-60ac505c65f662695c55 -->
-                    <!-- 使用的是v-list-item的title属性实现标题， prepend-icon实现hover之前展示icon-->
-                    <!-- 但是，在v-list-item中首先会渲染一个display为grid的块作为根，其类为v-list-item，下面有prepend和v-list-item__content，这两个块会并排存在 -->
-                    <!-- 那么，如果我简单的在v-list-item中添加一个card话，这个card会被放在v-list-item__content -->
-                    <!-- 最终样式就会变成 -->
-                    <!--      title -->
-                    <!-- icon card  -->
-                    <!--      card  -->
-
-                    <!-- 但我希望的样式是，也就是说我希望card能另起一行，不要和title挤在同一列里 -->
-                    <!-- icon title  -->
-                    <!-- c  a  r  d  -->
-                    <!-- c  a  r  d  -->
-                    <!-- 于是决定重写v-list-item的插槽自己实现，然后将默认插槽的div与v-card分开，div存储icon和title -->
-                    <!-- 但是者带来了一个新问题，hover nav之前，title会被展示在nav中而不会隐藏掉，为了解决这个问题，直接在div中加上了v-list-item这个类，让div具备grid属性，从而固定div中的位置，反复尝试的结果，因为我也不太记得grid定位的用法了，所以直接抄vuetify内置的class -->
-                    <v-list-item v-if="showTaskStartupDetail" key="2-1"
-                        @mouseleave="showTaskStartupDetail = !showTaskStartupDetail" link>
+                    <!-- 见vuetify-guide -->
+                    <v-list-item v-if="showTaskStartupDetail" key="2-1"  @mouseleave="updateShowTaskStartupDetail" link>
                         <template v-slot:default>
                             <div class="v-list-item pa-0">
                                 <v-progress-circular color="primary" :model-value="20" :size="25"
@@ -84,7 +84,7 @@ const rail = ref(true)
                         </template>
 
                     </v-list-item>
-                    <v-list-item key="2-2" v-else @mouseenter="showTaskStartupDetail = !showTaskStartupDetail">
+                    <v-list-item key="2-2" v-else @mouseenter="updateShowTaskStartupDetail">
                         <div class="v-list-item pa-0">
                             <v-progress-circular color="primary" :model-value="20" :size="25"
                                 class="mr-5"></v-progress-circular>
