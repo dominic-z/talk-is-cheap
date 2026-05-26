38bb06e4e59afb711d7128622bb7b9ccec2e7e2f
dominic_starry <dominic_starry@outlook.com>
2026-05-26 20:47:23 +0800

    ~

--- java-playground/java-tool-demo/transmittable-thread-local-demo/src/test/java/Example.java
+++ java-playground/java-tool-demo/transmittable-thread-local-demo/src/test/java/Example.java
@@ -8,27 +8,110 @@ import java.util.concurrent.TimeUnit;
 
 public class Example {
 
-    @Test
-    public void test() throws InterruptedException {
-        InheritableThreadLocal<String> itl = new InheritableThreadLocal<>();
-        itl.set("hello InheritableThreadLocal");
 
-        TransmittableThreadLocal<String> ttl = new TransmittableThreadLocal<>();
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+    @Test
+    public void testInheritableThreadLocal() throws InterruptedException {
+        ThreadLocal<String> tl = new ThreadLocal<>();
+        tl.set("hello ThreadLocal in Main");
+
+//        // 在主线程中设置InheritableThreadLocal的值
+        InheritableThreadLocal<String> itl = new InheritableThreadLocal<>();
+        itl.set("hello InheritableThreadLocal in Main");
+
+        // 创建子线程的瞬间，子线程会copy主线程的inheritableThreadLocals
+        Thread subThread = new Thread(()->{
+            System.out.println("ThreadLocal value in new thread: " + tl.get());
+            System.out.println("InheritableThreadLocal value in new thread: " + itl.get());
+            itl.set("hello InheritableThreadLocal in subThread");
+        });
+        subThread.start();
+        subThread.join();
+
+        System.out.println(tl.get());
+        System.out.println(itl.get()); // 子线程设置InheritableThreadLocal不会影响主线程
+
+    }
+
+
+    @Test
+    public void testInheritableThreadLocalWontHelpInThreadPool() throws InterruptedException {
+
+        // 前面的例子是主线程设置InheritableThreadLocal，然后创建子线程，这时子线程可以copy到主线程的InheritableThreadLocal
+        // 但是在线程池场景，线程是先创建的，后面在程序运行过程中，某个线程A即使设置了InheritableThreadLocal并提交任务到这个线程池，
+        // 线程池里的线程也无法获取这个线程A的InheritableThreadLocal，因为线程是先创建的
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        // 先提交一个任务，把这个线程创建起来，newFixedThreadPool并不会真正创建线程看源码
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        // 在主线程中设置InheritableThreadLocal的值
+        InheritableThreadLocal<String> itl = new InheritableThreadLocal<>();
+        itl.set("hello InheritableThreadLocal in Main");
+
+        // 在线程池中执行任务
+        executorService.execute(TtlRunnable.get(() -> {
+            System.out.println("InheritableThreadLocal value in new thread: " + itl.get()); // 结果是null，因为这个子线程是先创建的
+            itl.set("hello InheritableThreadLocal in subThread");
+        }));
+
+        // 等待任务执行完成
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.SECONDS);
+
+        System.out.println(itl.get());
+
+    }
+
+
+
+    @Test
+    public void testTransmittableThreadLocal() throws InterruptedException {
+
+        // ttl解决了线程池的问题
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1); // 设置为一个线程，
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
 
         // 在主线程中设置TransmittableThreadLocal的值
-        ttl.set("Hello, World!");
+        TransmittableThreadLocal<String> ttl = new TransmittableThreadLocal<>();
+        ttl.set("Hello, TransmittableThreadLocal in Main World!");
+        // 在主线程中设置InheritableThreadLocal的值
+        InheritableThreadLocal<String> itl = new InheritableThreadLocal<>();
+        itl.set("hello InheritableThreadLocal in Main");
 
         // 在线程池中执行任务
         executorService.execute(TtlRunnable.get(() -> {
             String value = ttl.get();
             System.out.println("TransmittableThreadLocal value in new thread: " + value);
             System.out.println("InheritableThreadLocal value in new thread: " + itl.get());
+            ttl.set("Hello, subThread World!");
+            itl.set("hello InheritableThreadLocal in subThread");
         }));
 
         // 等待任务执行完成
         executorService.shutdown();
         executorService.awaitTermination(1, TimeUnit.SECONDS);
+
+        System.out.println(ttl.get());
 
     }
 }
--- spring/spring-boot/hello-spring-boot/hello-spring-boot-starter-web/static-resources/src/main/java/resources/controller/FileDownloadController.java
+++ spring/spring-boot/hello-spring-boot/hello-spring-boot-starter-web/static-resources/src/main/java/resources/controller/FileDownloadController.java
@@ -2,6 +2,7 @@ package resources.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -41,6 +47,56 @@ public class FileDownloadController {
                 .headers(headers)
                 .contentLength(file.length())
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+
+    @GetMapping(value = "/download_file_streaming_via_response_entity", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    public ResponseEntity<InputStreamResource> downloadFileStreamingViaResponseEntity() throws IOException{
+        // 创建一个线程安全的管道，用于生产者(数据库读取)和消费者(HTTP响应)之间的通信
+        // https://www.qianwen.com/share/chat/a40dc4be820644619d64b3ee2cd4bed3
+        PipedInputStream pipedIn = new PipedInputStream();
+        PipedOutputStream pipedOut = new PipedOutputStream(pipedIn);
+
+        // 在新线程中执行耗时的数据读取和写入操作
+        Thread dataWriterThread = new Thread(() -> {
+            try (PrintWriter writer = new PrintWriter(new OutputStreamWriter(pipedOut, StandardCharsets.UTF_8))) {
+                writer.println("订单ID,订单名称,下单人");
+                writer.flush();
+
+                for (int i=0;i<100;i++) {
+                    String line = String.format("%d,%s,%s%n", i, "orderName:"+i, "orderPerson"+i+"name");
+                    writer.print(line);
+                    if (i % 10 == 0) {
+                        writer.flush();
+                    }
+                }
+                writer.flush();
+            } catch (Exception e) {
+                System.err.println("Error generating file content: " + e.getMessage());
+                e.printStackTrace();
+            } finally {
+                try {
+                    pipedOut.close(); // 关闭输出端，通知输入端没有更多数据
+                } catch (IOException e) {
+                    System.err.println("Error closing piped output stream: " + e.getMessage());
+                }
+            }
+        });
+
+        dataWriterThread.start();
+
+        // 创建Resource
+        InputStreamResource resource = new InputStreamResource(pipedIn);
+
+        // 设置响应头
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"orders_response_entity.txt\"");
+
+        // 返回ResponseEntity
+        return ResponseEntity.ok()
+                .headers(headers)
+//                .contentLength(-1) // 对于未知大小的流，可以设置为 -1
+                .contentType(MediaType.TEXT_PLAIN)
                 .body(resource);
     }
 }
